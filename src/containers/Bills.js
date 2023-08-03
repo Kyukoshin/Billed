01f94a6ba8389d307f1a71064f0a48d34f40bbda
RemiBertrand <rbertrand21@gmail.com>
2023-08-03 11:32:29 +0200

    First commit, fixed admin login and file extension

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -52,6 +52,13 @@ export default class {
               }
             }
           })
+          console.log(bills)
+          bills.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(b.date) - new Date(a.date);
+          });
+          console.log(bills)
           console.log('length', bills.length)
         return bills
       })
