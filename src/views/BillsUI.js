b374702b99337c64b579142871032ca08eacf072
RemiBertrand <rbertrand21@gmail.com>
2023-09-21 11:26:29 +0200

    Fixes to tests

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -22,7 +22,7 @@ const row = (bill) => {
 const rows = data => {
   // return (data && data.length) ? data.map(bill => row(bill)).join("") : "";
   return (data && data.length) ? data
-    .sort((a, b) => (a.date > b.date) ? 1 : -1)
+    .sort((a, b) => (a.date < b.date))
     .map(bill => row(bill))
     .join("") : "";
 };
